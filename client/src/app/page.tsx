7e9d82d488a7bab0701f01122a219f5a99ba8b56
ARTTTT_TTTT <tanapath.tp@gmail.com>
2025-05-09 13:20:10 +0700

    feat(client): structure detection page

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import '@/lib/env';
 
 export default function HomePage() {
@@ -10,6 +11,13 @@ export default function HomePage() {
       <span className='mt-2 text-gray-500 text-lg'>
         ระบบตรวจสอบการเข้า-ออกงานของพนักงาน
       </span>
+
+      <Link
+        href='/detection'
+        className='mt-8 inline-block rounded-lg bg-blue-500 px-4 py-2 text-white font-semibold hover:bg-blue-600 transition duration-200'
+      >
+        Detection Page
+      </Link>
 
       <footer className='absolute bottom-4 text-base text-gray-500'>
         © {new Date().getFullYear()} by{' '}
