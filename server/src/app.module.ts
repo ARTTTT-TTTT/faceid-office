b460f531dfd4ac5085c0f334355e8086b7d8cac0
peterpk1 <phatsawut.phuket1@gmail.com>
2025-05-16 15:07:24 +0700

    feat(server): add admin module with controller, service, and DTOs

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { PrismaService } from './prisma/prisma.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
+import { AdminModule } from './admin/admin.module';
 
 @Module({
-  imports: [PrismaModule, AuthModule],
+  imports: [PrismaModule, AuthModule, AdminModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
