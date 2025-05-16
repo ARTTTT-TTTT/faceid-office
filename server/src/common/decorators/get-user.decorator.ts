62ff465c901b11e8caf5fa7a9dcf04b4946dd795
peterpk1 <phatsawut.phuket1@gmail.com>
2025-05-16 15:57:49 +0700

    feat(server): enhance JWT payload to include admin name and update user decorator

--- server/src/common/decorators/get-user.decorator.ts
+++ server/src/common/decorators/get-user.decorator.ts
@@ -2,8 +2,12 @@ import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 import { Request } from 'express';
 
 export const GetUser = createParamDecorator(
-  (data: keyof any, ctx: ExecutionContext): unknown => {
-    const request: Request = ctx.switchToHttp().getRequest();
-    return data ? request.user?.[data] : request.user;
+  (data: keyof Request['user'] | undefined, ctx: ExecutionContext) => {
+    const request = ctx.switchToHttp().getRequest<Request>();
+    const user = request.user;
+
+    if (!user) return null;
+
+    return data ? user[data] : user;
   },
 );
