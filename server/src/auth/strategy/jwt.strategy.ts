d2befa5d0b9364ddddba995c9753ff7411f8df08
peterpk1 <phatsawut.phuket1@gmail.com>
2025-05-16 15:57:49 +0700

    feat(server): enhance JWT payload to include admin name and update user decorator

--- server/src/auth/strategy/jwt.strategy.ts
+++ server/src/auth/strategy/jwt.strategy.ts
@@ -13,6 +13,6 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     });
   }
   validate(payload: { sub: string; email: string; name: string }) {
-    return { sub: payload.sub, email: payload.email, name: payload.name };
+    return { adminId: payload.sub, email: payload.email, name: payload.name };
   }
 }
