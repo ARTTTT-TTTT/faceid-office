8377f4d96ebee6d2d96a045bf8879e2617a98761
peterpk1 <phatsawut.phuket1@gmail.com>
2025-05-16 12:03:40 +0700

    feat(server): implement authentication module with registration and login

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,10 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaService } from './prisma/prisma.service';
 import { PrismaModule } from './prisma/prisma.module';
-import { PersonModule } from './person/person.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [PrismaModule, PersonModule],
+  imports: [PrismaModule, AuthModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
---
+++ server/src/auth/auth.service.ts
@@ -0,0 +1,53 @@
+import {
+  ConflictException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import * as bcrypt from 'bcryptjs';
+import { JwtService } from '@nestjs/jwt';
+import { Admin } from '@prisma/client';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private prisma: PrismaService,
+    private jwt: JwtService,
+  ) {}
+
+  async register(email: string, username: string, password: string) {
+    const existingUser = await this.prisma.admin.findUnique({
+      where: { email },
+    });
+
+    if (existingUser) {
+      throw new ConflictException('Username is already taken');
+    }
+
+    const hashed = await bcrypt.hash(password, 10);
+
+    const admin = await this.prisma.admin.create({
+      data: {
+        email,
+        name: username,
+        passwordHash: hashed,
+      },
+    });
+
+    return admin;
+  }
+
+  async login(email: string, password: string) {
+    const admin: Admin | null = await this.prisma.admin.findUnique({
+      where: { email },
+    });
+    if (!admin) throw new UnauthorizedException('Admin not found');
+
+    const isMatch = await bcrypt.compare(password, admin.passwordHash);
+    if (!isMatch) throw new UnauthorizedException('Invalid credentials');
+
+    const token = this.jwt.sign({ sub: admin.id, username: admin.email });
+
+    return { access_token: token };
+  }
+}
