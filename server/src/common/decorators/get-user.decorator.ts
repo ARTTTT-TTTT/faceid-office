1e5b7df10a6e7410e1e0fee0708923462e812c61
peterpk1 <phatsawut.phuket1@gmail.com>
2025-05-16 16:38:39 +0700

    feat(server): implement JWT authentication for admin session duration updates

--- server/src/common/decorators/get-user.decorator.ts
+++ server/src/common/decorators/get-user.decorator.ts
@@ -1,13 +1,27 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 import { Request } from 'express';
 
+interface JwtPayload {
+  sub: string;
+  email: string;
+  name: string;
+  // add more fields if needed
+}
+
 export const GetUser = createParamDecorator(
-  (data: keyof Request['user'] | undefined, ctx: ExecutionContext) => {
+  <T extends keyof JwtPayload | undefined>(
+    data: T,
+    ctx: ExecutionContext,
+  ): T extends keyof JwtPayload ? JwtPayload[T] : JwtPayload => {
     const request = ctx.switchToHttp().getRequest<Request>();
-    const user = request.user;
+    const user = request.user as JwtPayload;
 
-    if (!user) return null;
+    if (data) {
+      return user[data] as T extends keyof JwtPayload
+        ? JwtPayload[T]
+        : JwtPayload;
+    }
 
-    return data ? user[data] : user;
+    return user as T extends keyof JwtPayload ? JwtPayload[T] : JwtPayload;
   },
 );
