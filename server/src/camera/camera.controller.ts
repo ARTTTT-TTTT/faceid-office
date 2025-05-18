dce5e3dfd84d3af7e888a8dee6e5b5480c1ecece
peterpk1 <phatsawut.phuket1@gmail.com>
2025-05-19 00:18:22 +0700

    feat(server): add updateCamera endpoint and UpdateCameraDto for camera updates

--- server/src/camera/camera.controller.ts
+++ server/src/camera/camera.controller.ts
@@ -1,9 +1,20 @@
-import { Body, Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
-import { CameraService } from './camera.service';
-import { CreateCameraDto } from './dto/create-camera.dto';
-import { GetUser } from '@/common/decorators/get-user.decorator';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Patch,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
+
 import { JwtAuthGuard } from '@/auth/guard/jwt-auth.guard';
 import { CheckOwnership } from '@/common/decorators/check-ownership.decorator';
+import { GetUser } from '@/common/decorators/get-user.decorator';
+
+import { CameraService } from './camera.service';
+import { CreateCameraDto } from './dto/create-camera.dto';
+import { UpdateCameraDto } from './dto/update-camera.dto';
 @UseGuards(JwtAuthGuard)
 @Controller('camera')
 export class CameraController {
@@ -30,5 +41,14 @@ export class CameraController {
     @Param('cameraId') cameraId: string,
   ) {
     return this.cameraService.getOneCamera(adminId, cameraId);
+  }
+
+  @CheckOwnership('camera', 'cameraId')
+  @Patch(':cameraId')
+  updateCamera(
+    @Param('cameraId') cameraId: string,
+    @Body() dto: UpdateCameraDto,
+  ) {
+    return this.cameraService.updateCamera(cameraId, dto);
   }
 }
