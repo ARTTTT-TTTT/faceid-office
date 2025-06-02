83ed9a5e8913547db00edd2f45b43ffb6326980a
ARTTTT_TTTT <tanapath.tp@gmail.com>
2025-06-02 16:55:06 +0700

    fix(root): improve branch cleanup logic in clean_branches.bat

--- scripts/clean_branches.bat
+++ scripts/clean_branches.bat
@@ -7,15 +7,15 @@ git remote prune origin
 
 for /f "delims=" %%A in ('git branch --show-current') do set CURRENT=%%A
 
-for /f "tokens=* delims= " %%A in ('git branch -r') do (
+if exist remote.tmp del remote.tmp
+for /f "delims=" %%A in ('git branch -r') do (
     set "line=%%A"
     call set "line=%%line:origin/=%%"
     echo !line!>> remote.tmp
 )
 
-for /f "tokens=* delims= " %%B in ('git branch') do (
+for /f "delims=" %%B in ('git for-each-ref --format="%%(refname:short)" refs/heads/') do (
     set "branch=%%B"
-    set "branch=!branch:~2!"
     if not "!branch!"=="%CURRENT%" if not "!branch!"=="main" (
         findstr /x /c:"!branch!" remote.tmp >nul
         if errorlevel 1 (
