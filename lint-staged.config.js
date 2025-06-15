bc882af0829f3fad89744c02e275fe0d6a48dae9
ARTTTT_TTTT <tanapath.tp@gmail.com>
2025-06-15 09:59:32 +0700

    fix(root): lint-staged error

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,14 +1,32 @@
+// * SOLUTION 1
+// module.exports = {
+//   'client/**/*.{js,ts,jsx,tsx}': [
+//     // 'pnpm --filter client format',
+//     'pnpm --filter client lint',
+//   ],
+//   'server/**/*.{js,ts,jsx,tsx}': [
+//     // 'pnpm --filter server format',
+//     'pnpm --filter server lint',
+//   ],
+//   'ai/**/*.py': [
+//     // 'cd ai && poetry run black .',
+//     'cd ai && poetry run ruff check . --fix',
+//   ],
+//   '*.{js,json,md}': 'prettier -w --ignore-unknown',
+// };
+
+// * SOLUTION 2
 module.exports = {
   'client/**/*.{js,ts,jsx,tsx}': [
-    // 'pnpm --filter client format',
-    'pnpm --filter client lint',
+    () => 'cd client && pnpm format',
+    () => 'cd client && pnpm lint',
   ],
   'server/**/*.{js,ts,jsx,tsx}': [
-    // 'pnpm --filter server format',
-    'pnpm --filter server lint',
+    () => 'cd server && pnpm format',
+    () => 'cd server && pnpm lint',
   ],
   'ai/**/*.py': [
-    // 'cd ai && poetry run black .',
+    'cd ai && poetry run black .',
     'cd ai && poetry run ruff check . --fix',
   ],
   '*.{js,json,md}': 'prettier -w --ignore-unknown',
